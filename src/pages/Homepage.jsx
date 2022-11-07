5cd0f4c98f2143ff98a27e882f76d4183e57baa7
KyoshiNoda <kyoshisew@gmail.com>
2022-11-07 12:04:00 -0500

    added basic homepage layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Homepage() {
   return (
-    <div>
-     Homepage
+    <div className="flex justify-center">
+      <div className="flex justify-evenly items-center h-48 w-96 bg-blue-600">
+        <div className="flex justify-center items-center bg-green-600 w-32 h-32 rounded text-2xl">
+          PROS
+        </div>
+        <div className="flex justify-center items-center bg-red-600 w-32 h-32 rounded text-2xl">
+          CONS
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Homepage;
