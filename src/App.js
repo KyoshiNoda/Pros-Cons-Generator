f3ab64f6c28e97504092b2369b1485be15cdf5bf
KyoshiNoda <kyoshisew@gmail.com>
2022-11-07 11:49:56 -0500

    installed react routes and basic pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Group from "./pages/Group";
+import List from "./pages/List";
 function App() {
   return (
-    <div className='bg-red-900'>
-      hello
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Group" element={<Group />} />
+        <Route path = "/List" element = {<List/>}/>
+      </Routes>
+    </>
   );
 }
 
