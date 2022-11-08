4a739ced0c8ee19532165ad2b6fe8a7c96d5a6a0
KyoshiNoda <kyoshisew@gmail.com>
2022-11-08 12:09:41 -0500

    added create modal

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-
+import MakeModal from "../components/CreateModal";
 function Homepage() {
   return (
     <div className="flex justify-center">
-      <div className="flex justify-evenly items-center h-48 w-96 bg-blue-600">
+      <div className="flex justify-evenly items-center h-48 w-96 bg-gradient-to-r from-cyan-500 to-blue-500">
         <div className="flex justify-center items-center bg-green-600 w-32 h-32 rounded text-2xl">
-          PROS
+          <MakeModal/>
         </div>
-        <div className="flex justify-center items-center bg-red-600 w-32 h-32 rounded text-2xl">
-          CONS
-        </div>
+
+        <a href="/List">
+          <div className="flex justify-center items-center bg-red-600 w-32 h-32 rounded text-2xl">
+            View
+          </div>
+        </a>
       </div>
     </div>
   );
