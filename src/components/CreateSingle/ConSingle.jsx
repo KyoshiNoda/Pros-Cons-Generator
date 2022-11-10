51e70d8a24589463c48fec206200de878f8b9d89
KyoshiNoda <kyoshisew@gmail.com>
2022-11-10 16:58:25 -0500

    added input with useState

--- src/components/CreateSingle/ConSingle.jsx
+++ src/components/CreateSingle/ConSingle.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ConSingle() {
+  const [input, setInput] = useState("");
+  const inputHandler = (event) => {
+    setInput(event.target.value);
+    console.log(input);
+  };
   return (
-    <div className="flex flex-col bg-red-500 h-full w-1/2 rounded">
-      <div className="text-center bg-slate-700">
-        <h1 className="text-4xl text-white items-start">Pros</h1>
+    <div className="flex flex-col bg-red-500 h-full w-1/2 rounded p-5 gap-y-5">
+      <div className="text-center">
+        <h1 className="text-4xl text-white items-start">Cons</h1>
       </div>
-      <button
-        type="button"
-        class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-      >
-        Add
-      </button>
+      <div className="bg-slate-100 h-full w-full rounded"></div>
+      <div className="flex justify-center">
+        <input type="text" value={input} onChange={inputHandler} />
+      </div>
     </div>
   );
 }
--- src/pages/CreateSingle.jsx
+++ src/pages/CreateSingle.jsx
@@ -17,7 +17,7 @@ function CreateSingle() {
 
           <button
             type="button"
-            class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
           >
             Submit
           </button>
