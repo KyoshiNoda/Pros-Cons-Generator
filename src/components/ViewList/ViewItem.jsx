503dbd442debcf77d77b0d9747c096a774699fcf
unknown <kyoshisew@gmail.com>
2022-11-10 22:46:41 -0500

    added color themes

--- src/components/ViewList/ViewItem.jsx
+++ src/components/ViewList/ViewItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ViewItem(props) {
   return (
-    <a href = "/ViewSingle" className="bg-green-500 w-40 h-40 flex justify-center items-center rounded-full overflow-hidden shadow-lg">
+    <a href = "/ViewSingle" className="bg-cyan-500 w-40 h-40 flex justify-center items-center rounded-full overflow-hidden shadow-lg hover:bg-cyan-700">
         <h1 className='text-white font-bold'>{props.name}</h1>
     </a>
   )
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,8 +3,11 @@ import CreateModal from "../components/CreateModal";
 import ViewModal from "../components/ViewModal";
 function Homepage() {
   return (
-    <div className="flex justify-center">
-      <div className="flex justify-evenly items-center h-screen w-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div className="flex flex-col justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
+      <div className="flex justify-center">
+        <h1 className="text-white text-6xl font-bold">Pros Cons Generator</h1>
+      </div>
+      <div className="flex justify-evenly items-center h-screen w-screen">
           <CreateModal/>
           <ViewModal/>
       </div>
