68ecf30724b67d6bbfc2f6f559c2ffa7a177e6b0
KyoshiNoda <kyoshisew@gmail.com>
2022-11-15 10:45:17 -0500

    added modal

--- src/pages/CreateSingle.jsx
+++ src/pages/CreateSingle.jsx
@@ -7,17 +7,17 @@ import Card from '../components/Card';
 function CreateSingle() {
   return (
     <Card>
-        <div className="flex flex-col h-1/2 w-1/2 bg-slate-400 rounded items-center gap-y-5">
-          <h1 className="text-4xl text-white ">Create Single</h1>
+        <div className="flex flex-col h-5/6 w-5/6 bg-slate-400 rounded items-center gap-y-5">
+          <h1 className="text-3xl lg:text-5xl text-white font-bold">Create Single</h1>
 
           <div className="flex h-3/4 w-3/4">
             <ProSingle />
             <ConSingle />
-          </div>
+          </div>  
 
           <button
             type="button"
-            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2"
           >
             Submit
           </button>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CreateModal from "../components/CreateModal";
+import InfoModal from "../components/InfoModal";
 import ViewModal from "../components/ViewModal";
 function Homepage() {
   return (
     <div className="flex flex-col justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
+      <InfoModal title = "Currently Working!"/>
       <div className="flex justify-center">
         <h1 className="text-white text-6xl font-bold">Pros Cons Generator</h1>
       </div>
