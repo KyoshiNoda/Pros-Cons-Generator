2d6360cd01037c46ba00037fd983d065fb6ecc5f
KyoshiNoda <kyoshisew@gmail.com>
2022-11-28 12:23:06 -0500

    added db to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app  = express();
+const mysql = require('mysql');
 
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "Pros-Cons"
+});
 
 app.get('/', (req,res)=>{
-  res.send('This is running on port 3001');
+  res.send('this is running with NODEMON');
 });
 app.listen(3001, () =>{
   console.log('active on port 3001');
